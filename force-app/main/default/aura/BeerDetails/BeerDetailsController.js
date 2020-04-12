d841690c44ea9d8e02b7233641bd309295ceb4d3
hrshtagg <hrshtagg@gmail.com>
2020-04-13 01:50:13 +0530

    Day 10.2

--- force-app/main/default/aura/BeerDetails/BeerDetailsController.js
+++ force-app/main/default/aura/BeerDetails/BeerDetailsController.js
@@ -1,5 +1,16 @@
 ({
-    myAction : function(component, event, helper) {
-
+    doOrder : function(component, event, helper) {
+        
+        var pageRef = component.find('navigation');
+        var pageReferenceNav = {
+            type: 'standard__component',
+            attributes: {
+                componentName : 'c__CreateBeerOrder'
+            },
+            state: {
+                
+            }
+        };
+        pageRef.navigate(pageReferenceNav);
     }
 })
--- force-app/main/default/aura/BeerListComponent/BeerListComponentController.js
+++ force-app/main/default/aura/BeerListComponent/BeerListComponentController.js
@@ -5,5 +5,42 @@
         console.log("method " + beerObj);
         component.set('v.beerId', beerObj);
         console.log("method ends " + 'v.beerId');
+        console.log("In button");
+        $A.createComponent(
+            "c:BeerDetails",
+            {
+                "beerId": beerObj,
+            },
+            function(beerDetails, status, errorMessage){
+                //Add the new button to the body array
+                if (status === "SUCCESS") {
+                    console.log("In SUCCESS");
+                    component.find('overlayLib').showCustomModal({
+                        header: "Beer Details",
+                        body: beerDetails,
+                        footer: "in footer",  	
+                        showCloseButton: true,
+                        cssClass: "mymodal",
+                        
+                    })
+                    
+                }
+                else if (status === "INCOMPLETE") {
+                    console.log("No response from server or client is offline.")
+                    // Show offline error
+                }
+                    else if (status === "ERROR") {
+                        console.log("Error: " + errorMessage);
+                        // Show error message
+                    }
+            }
+        );
+    },
+    
+    handlePress : function(cmp) {
+        // Find the button by the aura:id value
+        console.log("button: " + cmp.find("findableAuraId"));
+        console.log("button pressed");
     }
+    
 })
